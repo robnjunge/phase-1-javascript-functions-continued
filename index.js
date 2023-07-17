e811e6bcd5583459ab041fc27c7b4ef606024c71
robertnjunge <mubearobert@gmail.com>
2023-07-17 17:28:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+// // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return`This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (work = "go to the office") {
+    return`This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(asteric = '*') {
+    const newThing = function (adjective = "Beautiful") {
+        return `You are ${asteric}${adjective}${asteric}!`;
+    };
+    return newThing;
+    
+}
+const result = wrapAdjective('||');
+
+
